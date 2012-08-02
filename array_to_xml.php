48c64a5edaa224828819337017da086b68c8ed86
Victor Poklonskiy <waracolyte@gmail.com>
2012-08-02 11:08:58 +0300

    Update array_to_xml.php

--- array_to_xml.php
+++ array_to_xml.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * convert array to xml
- * @link https://github.com/P54l0m5h1k/array_to_xml
- * @link https://github.com/P54l0m5h1k/xml_to_array - twin brother
+ * @link https://github.com/P54l0m5h1k/Array-to-XML-PHP/
+ * @link https://github.com/P54l0m5h1k/XML-to-Array-PHP/- twin brother
  */
 class array_to_xml
 {
